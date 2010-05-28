73c9c4659c1604b34bb744a08ac1aa7f449c6139
sobomax <sobomax@sippysoft.com>
2010-05-28 03:17:25 -0700

    Remove now disfunctional CVS Id keyword.

--- setup.py
+++ setup.py
@@ -23,8 +23,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
-#
-# $Id: setup.py,v 1.2 2009/08/13 21:02:12 sobomax Exp $
 
 from distutils.core import setup
 
