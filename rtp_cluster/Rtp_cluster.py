db3142d887a5894de11567790a23db89644dcc49
sobomax <sobomax@sippysoft.com>
2012-08-09 18:13:17 -0700

    Don't import Rtp_cluster_member class, we are not using it here.

--- rtp_cluster/Rtp_cluster.py
+++ rtp_cluster/Rtp_cluster.py
@@ -21,7 +21,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 
-from Rtp_cluster_member import Rtp_cluster_member
 from twisted.internet import reactor
 
 import sys
