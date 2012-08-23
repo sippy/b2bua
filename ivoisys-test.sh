d1b6037dbbbae1c8ad766871605b62c89c6b96ba
Maksym Sobolyev <sobomax@sippysoft.com>
2012-08-23 13:29:53 +1000

    Add sip_proxy and nat_traversal options.

--- ivoisys-test.sh
+++ ivoisys-test.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-sudo python2.6 b2bua_radius.py -f -u -s 'pennytel.com;op=202.85.245.137;auth=8889203691:sobomax123' -A 0 --rtp_proxy_clients="/var/run/rtpproxy.sock"
+sudo python2.6 b2bua_radius.py -f -u -s 'pennytel.com;op=202.85.245.137;auth=8889203691:sobomax123' \
+  -A 0 --rtp_proxy_clients="/var/run/rtpproxy.sock" --sip_proxy=202.85.245.137 --nat_traversal=on
