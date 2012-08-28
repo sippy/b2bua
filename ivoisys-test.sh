5ab8e4bc04d614c511915c7a01acf070fdf12acd
Maksym Sobolyev <sobomax@sippysoft.com>
2012-08-28 12:04:38 +1000

    Adjust parameters.

--- ivoisys-test.sh
+++ ivoisys-test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sudo python2.6 b2bua_radius.py -f -u -s 'pennytel.com;op=202.85.245.137;auth=8889203691:sobomax123' \
+sudo python2.6 b2bua_radius.py --digest_auth_only=on \
   -A 0 --rtp_proxy_clients="/var/run/rtpproxy.sock" --sip_proxy=202.85.245.137 --nat_traversal=on
