fadef96307c807850328e7f9b8688ebe475dc541
Maksym Sobolyev <sobomax@sippysoft.com>
2012-08-31 23:57:22 +1000

    Add SYSLOG_BEND.

--- ivoisys-back.sh
+++ ivoisys-back.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-sudo python2.6 b2bua_radius.py --digest_auth=off --accept_ips=202.85.245.137 \
+SIPLOG_BEND=syslog
+
+python2.6 b2bua_radius.py --digest_auth=off --accept_ips=202.85.245.137 \
   -A 0 -s 202.85.243.19 --auth_enable=off \
   --pass_headers=Remote-Party-ID,Allow,Supported
