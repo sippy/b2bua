8a43970c3642c0c4c722e597eda538f23dac2fb2
jevonearth <ebjorsell@gmail.com>
2014-09-12 14:11:21 -0700

    Update setup.py to use entry_point console_scripts, pull in github's default gitignore file for python projects.

--- setup.py
+++ setup.py
@@ -1,39 +1,27 @@
-#!/usr/local/bin/python
-# Copyright (c) 2003-2005 Maxim Sobolev. All rights reserved.
-# Copyright (c) 2006-2014 Sippy Software, Inc. All rights reserved.
-#
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without modification,
-# are permitted provided that the following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice, this
-# list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-# this list of conditions and the following disclaimer in the documentation and/or
-# other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#!/usr/bin/env python2
+from setuptools import setup, find_packages
 
-from distutils.core import setup
+setup(
+  name = "sippy",
+  version = "1.1-dev",
+  packages = find_packages(),
 
-setup(name = 'sippy',
-  version = '1.1-dev',
-  description = 'SIP RFC3261 Back-to-back User Agent (B2BUA)',
-  author = 'Sippy Software, Inc.',
-  author_email = 'sales@sippysoft.com',
-  url = 'http://www.b2bua.org/',
-  packages = ['sippy'],
-  scripts = ['sippy/b2bua_radius.py', 'sippy/b2bua_simple.py'],
-  data_files = [('etc/sippy', ['sippy/dictionary']),
-                ('share/doc/sippy', ['COPYING'])])
+  install_requires = ['twisted>=14.0'],
+
+  package_data = { '': ['dictionary', '*.md'] },
+
+  entry_points = {
+      'console_scripts': [
+          'b2bua_simple = sippy.b2bua_simple:main_func',
+          'b2bua_radius = sippy.b2bua_radius:main_func',
+          ],
+      },
+
+  # meta-data for upload to PyPi
+  author = "Sippy Software, Inc.",
+  author_email = "sales@sippysoft.com",
+  description = "SIP RFC3261 Back-to-back User Agent (B2BUA)",
+  license = "BSD",
+  keywords = "sip b2bua voip rfc3261 sippy",
+  url = "http://www.b2bua.org/",
+)
