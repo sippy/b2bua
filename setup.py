ab952fd0d8763749c655c1cd47b865212bcffd2d
jevonearth <ebjorsell@gmail.com>
2014-09-12 22:11:15 -0700

    Port shell based tests from self-test to proper unittests. Fix spacing. `./setup.py test` now works

--- setup.py
+++ setup.py
@@ -2,26 +2,31 @@
 from setuptools import setup, find_packages
 
 setup(
-  name = "sippy",
-  version = "1.1-dev",
-  packages = find_packages(),
+    name = "sippy",
+    version = "1.1-dev",
+    packages = find_packages(),
 
-  install_requires = ['twisted>=14.0'],
+    install_requires = [
+        'twisted>=14.0'
+        ],
 
-  package_data = { '': ['dictionary', '*.md'] },
+    package_data = {
+        '': ['dictionary', '*.md']
+        },
+    test_suite = 'tests',
 
-  entry_points = {
-      'console_scripts': [
-          'b2bua_simple = sippy.b2bua_simple:main_func',
-          'b2bua_radius = sippy.b2bua_radius:main_func',
-          ],
-      },
+    entry_points = {
+        'console_scripts': [
+            'b2bua_simple = sippy.b2bua_simple:main_func',
+            'b2bua_radius = sippy.b2bua_radius:main_func',
+            ],
+        },
 
-  # meta-data for upload to PyPi
-  author = "Sippy Software, Inc.",
-  author_email = "sales@sippysoft.com",
-  description = "SIP RFC3261 Back-to-back User Agent (B2BUA)",
-  license = "BSD",
-  keywords = "sip b2bua voip rfc3261 sippy",
-  url = "http://www.b2bua.org/",
+    # meta-data for upload to PyPi
+    author = "Sippy Software, Inc.",
+    author_email = "sales@sippysoft.com",
+    description = "SIP RFC3261 Back-to-back User Agent (B2BUA)",
+    license = "BSD",
+    keywords = "sip b2bua voip rfc3261 sippy",
+    url = "http://www.b2bua.org/",
 )
