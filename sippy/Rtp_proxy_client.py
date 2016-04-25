0bbc6f2d62b6473408cea2542b8ef1be38dea814
jevonearth <ebjorsell@gmail.com>
2016-04-25 11:21:55 -0700

    Remove blank line as per code review feedback.

--- sippy/Rtp_proxy_client.py
+++ sippy/Rtp_proxy_client.py
@@ -88,7 +88,6 @@ class Rtp_proxy_client(Rtp_proxy_client_udp, Rtp_proxy_client_stream):
         if kwargs.has_key('no_version_check'):
             no_version_check = kwargs['no_version_check']
             del kwargs['no_version_check']
-
         if len(address) == 0 and kwargs.has_key('spath'):
             a = kwargs['spath']
             del kwargs['spath']
