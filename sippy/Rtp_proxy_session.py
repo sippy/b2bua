002aa400f04306d444ab0721fca8f95a79044ad4
Maksym Sobolyev <sobomax@sippysoft.com>
2022-11-18 10:24:24 -0800

    Implement basic RTP->SRTP/DTLS gatewaying using dtls_gw module in the RTPProxy 3.0 and up.
    Just a backend for now.
    
    It has proven to be working in all feasible modes:
    
        DTLS->proxy->DTLS
        RTP->dtls_gw->DTLS
        DTLS->dtls_gw->RTP
        DTLS_1->dtls_gw->DTLS_2
    
    Some hookup into B2BUA application is to follow-up.

--- sippy/Rtp_proxy_session.py
+++ sippy/Rtp_proxy_session.py
@@ -105,6 +105,10 @@ class update_result():
     dtls_mode = None
     dtls_fingerprint = None
 
+DTLS_TRANSPORTS = ('UDP/TLS/RTP/SAVP', 'UDP/TLS/RTP/SAVPF')
+DTLS_ATTRS = ('setup', 'fingerprint', 'rtcp', 'ssrc')
+DTLS_ATTRS_RM = tuple([a for a in DTLS_ATTRS if a != 'ssrc'])
+
 class _rtpps_side(object):
     session_exists = False
     codecs = None
@@ -113,18 +117,21 @@ class _rtpps_side(object):
     origin = None
     oh_remote = None
     repacketize = None
+    gateway_dtls = 'pass'
     soft_repacketize = False
     after_sdp_change = None
     needs_new_port = False
+    transports = None
     rinfo_hst = None
 
     def __init__(self, name):
         self.origin = SdpOrigin()
         self.name = name
+        self.transports = {}
         self.rinfo_hst = []
 
     def __str__(self):
-        return f'_rtpps_side("name={self.name}")'
+        return f'_rtpps_side("name={self.name},transports={self.transports}")'
 
     def update(self, up):
         command = 'U'
@@ -180,11 +187,18 @@ class _rtpps_side(object):
         if result == None:
             up.result_callback(None, up.rtpps, *up.callback_parameters)
             return
-        t1 = result.split()
+        t0 = result.split('&&', 1)
+        t1 = t0[0].split()
         if t1[0][0] == 'E':
             up.result_callback(None, up.rtpps, *up.callback_parameters)
             return
         ur = update_result()
+        if len(t0) > 1:
+            subc_res = t0[1].lstrip()
+            if subc_res == '-1':
+                up.result_callback(None, up.rtpps, *up.callback_parameters)
+                return
+            ur.dtls_mode, ur.dtls_fingerprint = subc_res.split(' ', 1)
         ur.rtpproxy_port = int(t1[0])
         if ur.rtpproxy_port == 0:
             up.result_callback(None, up.rtpps, *up.callback_parameters)
@@ -265,7 +279,9 @@ class _rtpps_side(object):
             options = 'z%d' % self.repacketize
         else:
             options = ''
+        otherside = self.getother(rtpps)
         for si, sect in enumerate(sects):
+            self.transports[si] = sect.m_header.transport
             if sect.c_header.atype == 'IP6':
                 sect_options = '6' + options
             else:
@@ -278,6 +294,30 @@ class _rtpps_side(object):
             up.options = sect_options
             up.index = si
             up.result_callback = self._sdp_change_finish
+            if otherside.gateway_dtls == 'dtls':
+                up.subcommands.append('M4:1 S')
+                otherside.transports[si] = 'UDP/TLS/RTP/SAVP'
+            if self.gateway_dtls == 'dtls' and sect.m_header.transport in DTLS_TRANSPORTS:
+                adict = dict([(x.name, x.value) for x in sect.a_headers
+                              if x.name in DTLS_ATTRS])
+                if 'setup' not in adict:
+                    raise SdpParseError('Missing DTLS connection mode parameter')
+                if 'fingerprint' not in adict:
+                    raise SdpParseError('Missing DTLS fingerprint parameter')
+                asetup = adict['setup']
+                if asetup in ('active', 'actpass'):
+                    subcommand = 'M4:1 A'
+                elif asetup in ('passive',):
+                    subcommand = 'M4:1 P'
+                else:
+                    raise SdpParseError(F'Unknown connection mode: "{asetup}"')
+                subcommand += F' {adict["fingerprint"]}'
+                if 'ssrc' in adict:
+                    ssrc = adict['ssrc'].split(None, 1)[0]
+                    subcommand += F' {ssrc}'
+                up.subcommands.append(subcommand)
+                if otherside.gateway_dtls == 'rtp':
+                    otherside.transports[si] = 'RTP/AVP'
             up.callback_parameters = (sdp_body, sect, sects, result_callback)
             self.update(up)
         return
@@ -285,8 +325,21 @@ class _rtpps_side(object):
     def _sdp_change_finish(self, ur, rtpps, sdp_body, sect, sects, result_callback):
         sect.needs_update = False
         if ur != None:
+            otherside = self.getother(rtpps)
             if self.after_sdp_change != None:
                 self.after_sdp_change(ur.rtpproxy_address) # pylint: disable=not-callable
+            si = sects.index(sect)
+            if si in otherside.transports and self.gateway_dtls != 'pass' \
+              and otherside.transports[si] != sect.m_header.transport:
+                sect.m_header.transport = otherside.transports[si]
+                for dtls_hdr in [x for x in sect.a_headers
+                                 if x.name in DTLS_ATTRS_RM]:
+                    sect.a_headers.remove(dtls_hdr)
+            if ur.dtls_fingerprint:
+                for dtls_hdr in [x for x in sect.a_headers if x.name in DTLS_ATTRS_RM]:
+                    sect.a_headers.remove(dtls_hdr)
+                sect.addHeader('a', F'setup:{ur.dtls_mode}')
+                sect.addHeader('a', F'fingerprint:{ur.dtls_fingerprint}')
             sect.c_header.atype = ur.family
             sect.c_header.addr = ur.rtpproxy_address
             if sect.m_header.port != 0:
