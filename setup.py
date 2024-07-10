7a7526ab869859b00a10138ce5be4ab54002278a
Maksym Sobolyev <sobomax@sippysoft.com>
2024-07-10 15:07:56 -0700

    Bump version number.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name = "sippy",
-    version = "2.1.1",
+    version = "2.2.0",
     packages = find_packages(),
 
     install_requires = requirements,
