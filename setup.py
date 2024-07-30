aceca3a5cdadbd2e497ba3db957238fa6c04cf34
Maksym Sobolyev <sobomax@sippysoft.com>
2024-07-30 15:22:51 -0700

    Make this 2.3.0.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name = "sippy",
-    version = "2.2.0",
+    version = "2.3.0",
     packages = find_packages(),
 
     install_requires = requirements,
