d8b2d1e10b56399af50df78e742292795f2c85ad
Maksym Sobolyev <sobomax@sippysoft.com>
2025-11-04 21:59:13 -0800

    Set MM_INIT_DELAY.
    Ignore failure of the early_cancel_lost100 and early_cancel on architectures
    where it does not work reliably (yet).

--- docker/set_env.sh
+++ docker/set_env.sh
@@ -4,19 +4,35 @@ set -e
 set -x
 
 platformopts() {
+  local IMAGE_TAG="${BASE_IMAGE#*:}"
+  local _OS_TAG="${IMAGE_TAG%%_*}"
+  local OS_TAG="${_OS_TAG#*-}"
+
   case "${TARGETPLATFORM}" in
   linux/arm/v5)
     echo "RTPP_VERSION=production"
+    echo "MM_INIT_DELAY=6"
+    test "${OS_TAG}" != "debian" || echo "TEST_SET_MIGHTFAIL=early_cancel_lost100,early_cancel"
     ;;
   linux/arm/v7)
     echo "RTPP_VERSION=production"
     echo "OPENSSL_CONFIGURE_ARGS=linux-armv4"
+    echo "MM_INIT_DELAY=6"
     ;;
   linux/s390x)
     echo "RTPP_VERSION=production"
+    echo "MM_INIT_DELAY=6"
     ;;
   linux/arm64)
     echo "QEMU_CPU=cortex-a53"
+    echo "MM_INIT_DELAY=6"
+    ;;
+  linux/ppc64le)
+    echo "MM_INIT_DELAY=6"
+    ;;
+  linux/riscv64)
+    echo "MM_INIT_DELAY=6"
+    echo "TEST_SET_MIGHTFAIL=early_cancel_lost100"
     ;;
   esac
   echo 'EXTRA_PACKAGES="build-essential libunwind-dev git python3-dev"'
